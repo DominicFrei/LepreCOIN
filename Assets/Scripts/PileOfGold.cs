3f88192b1dffbcb0083c432ff6a923ff486057eb
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-13 16:59:48 +0100

    Let the tree leprechaun move and collect coins.

---
+++ Assets/Scripts/PileOfGold.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PileOfGold : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private int _amount = 0;
+
+    private void Start()
+    {
+        _text.text = "0";
+    }
+
+    public void GoldDelivered()
+    {
+        _amount++;
+        _text.text = _amount + "";
+    }
+}
---
+++ Assets/Scripts/TreeLeprechaun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class TreeLeprechaun : MonoBehaviour
+{
+    [SerializeField] private GameObject _goldCoin;
+    [SerializeField] private PileOfGold _pileOfGold;
+
+    private enum MovementState
+    {
+        None,
+        Left,
+        Right
+    }
+
+    private Vector3 _startingPosition;
+    private MovementState _movementState = MovementState.None;
+    private float _maximumMovement = 0.85f;
+
+    private void Start()
+    {
+        _startingPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        switch (_movementState)
+        {
+            case MovementState.Right:
+                transform.position += Vector3.right * Time.deltaTime;
+                if (transform.position.x >= _startingPosition.x + _maximumMovement)
+                {
+                    _goldCoin.SetActive(true);
+                    _movementState = MovementState.Left;
+                }
+                break;
+            case MovementState.Left:
+                transform.position += Vector3.left * Time.deltaTime;
+                if (transform.position.x <= _startingPosition.x)
+                {
+                    _goldCoin.SetActive(false);
+                    _pileOfGold.GoldDelivered();
+                    _movementState = MovementState.None;
+                }
+                break;
+        }
+
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (MovementState.None == _movementState)
+        {
+            _movementState = MovementState.Right;
+        }
+    }
+
+}
